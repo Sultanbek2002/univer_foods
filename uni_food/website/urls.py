a1a1f9eab5b65f10e3fc08a0cb46a338eedf55c2
Sultanbek2002 <https://github.com/Sultanbek2002>
2023-04-26 15:07:15 +0600

    Добавление профиля и добавление войти

--- uni_food/website/urls.py
+++ uni_food/website/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', index, name='home'),
     path('sign_up/', register, name='sign_up'),
     path('examination/', examination, name='examination'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('profile/', profile, name='profile'),
+    path('login/', loginUser, name='login'),
+    path('create_staff/', create_staff, name='create_staff')
 ]
--- uni_food/website/views.py
+++ uni_food/website/views.py
@@ -15,21 +15,41 @@ def index(request):
         if 'password' in request.COOKIES and 'data' in request.COOKIES:
             data = request.COOKIES.get('data')
             dataAll = json.loads(data)
+
+            alldata = json.loads(data)
+            print(alldata)
             if password == request.COOKIES.get('password'):
                 User.objects.create_user(username=dataAll['username'], first_name=dataAll['name'],
                                          last_name=dataAll['surname'],
-                                         email=dataAll['email'], passwor=dataAll['password'])
-                user = authenticate(request, username=dataAll['username'], password=dataAll['password'])
+                                         email=dataAll['email'], password=dataAll['password'])
+                user = authenticate(
+                    request, username=dataAll['username'], password=dataAll['password'])
                 login(request, user)
                 return redirect('home')
             else:
-                messages.error('Вы ввели неправильный код. Пожалуйсто проверьте почту!')
-                return redirect('examination')
+                messages.error(
+                    request, 'Вы ввели неправильный код. Пожалуйсто проверьте почту!')
+            #     return redirect('examination')
     return render(request, 'website/index.html')
 
 
 def register(request):
+
     return render(request, 'website/register.html')
+
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Что-то пошло не так, повторите попытку.')
+            return redirect('login')
+    return render(request, 'website/login.html')
 
 
 def examination(request):
@@ -48,26 +68,36 @@ def examination(request):
         if password == re_password:
             print('password')
             if username == User.objects.filter(username=username):
-                messages.error('Вы не можете добавлять, так как такой пользователь уже существует!!! ')
-                return redirect('register')
+                messages.error(
+                    request, 'Вы не можете добавлять, так как такой пользователь уже существует!!! ')
+                return redirect('sign_up')
             elif email == User.objects.filter(email=email):
-                messages.error('Пользователь с такой почтой уже есть!! Поменяйте почту.')
-                return redirect('register')
+                messages.error(
+                    request, 'Пользователь с такой почтой уже есть!! Поменяйте почту.')
+                return redirect('sign_up')
             else:
                 send_mail(
                     'ITkg',
-                    f'Hello {name}',
+                    f'Подтвердите этот код {arg} ',
                     'setting.EMAIL_HOST_USER',
                     [email],
                     fail_silently=False
                 )
-                response = render(request, 'website/index.html')
+                response = render(request, 'website/exanmination.html')
                 response.set_cookie('password', arg)
-                response.set_cookie('data', data)
+                response.set_cookie('data', json.dumps(data))
                 return response
         else:
-            messages.error(
-                'Повторный пароль не совподает прежним паролем!! Обратите внимание они должны быть одиноковыми.')
-            return redirect('register')
+            messages.error(request,
+                           'Повторный пароль не совподает прежним паролем!! Обратите внимание они должны быть одиноковыми.')
+            return redirect('sign_up')
     else:
         return redirect('examination')
+
+
+def profile(requests):
+    return render(requests, 'website/profile.html')
+
+
+def create_staff(request):
+    return render(request, 'website/create_staff.html')
